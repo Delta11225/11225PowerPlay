f5fa3160a6b590f1e3a0ec48e63f224bc77e1c21
Delta11225 <lcsdeltarobotics@gmail.com>
2021-10-22 10:48:55 -0400

    Teleop downloadable version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetectionWebcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetectionWebcamTest.java
@@ -51,7 +51,7 @@ import java.util.List;
  * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
  * is explained below.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
+@TeleOp(name = "Tensor Flow", group = "Concept")
 public class TensorFlowObjectDetectionWebcamTest extends LinearOpMode {
   /* Note: This sample uses the all-objects Tensor Flow model (FreightFrenzy_BCDM.tflite), which contains
    * the following 4 detectable objects
