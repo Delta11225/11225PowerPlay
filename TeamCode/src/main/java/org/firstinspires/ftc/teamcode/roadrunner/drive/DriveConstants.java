f7b4ae6fd250e4f10c23740a110c5a2160941702
Delta11225 <lcsdeltarobotics@gmail.com>
2021-11-03 11:54:28 -0400

    Updated Track Width and kP for heading and translational Deleted old files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1.0; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 10.70866; // in
+    public static double TRACK_WIDTH = 13.75; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
