1b294fad6ef7e8c5c03b01b17074b392f80e23a8
Delta11225 <lcsdeltarobotics@gmail.com>
2022-03-29 10:52:28 -0400

    Added teleop for testing purposes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/testing/TeleopTestSound.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.teleop.testing;
+
+import com.qualcomm.ftccommon.SoundPlayer;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TeleopTestSound extends LinearOpMode {
+    boolean isX = false;
+    boolean wasX = false;
+    boolean bruhFound = false;
+    int bruhID = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int bruhId = hardwareMap.appContext.getResources().getIdentifier("bruh", "raw", hardwareMap.appContext.getPackageName());
+
+        if (bruhID != 0)
+            bruhFound = SoundPlayer.getInstance().preload(hardwareMap.appContext, bruhId);
+
+        telemetry.addData("sound resource", bruhFound ? "Found" : "NOT found\n Add bruh.mp3 to /src/main/res/raw" );
+
+        telemetry.addData(">", "Press X to play sounds.");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+
+            if (bruhFound && (isX = gamepad1.x) && !wasX) {
+                SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, bruhID);
+                telemetry.addData("Playing", "bruh");
+                telemetry.update();
+            }
+
+            wasX = isX;
+        }
+    }
+}
