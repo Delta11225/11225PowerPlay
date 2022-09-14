49af502d70a3af285bac3d1ff4aaaf3eeea3d39a
Delta11225 <lcsdeltarobotics@gmail.com>
2022-09-14 13:36:50 -0400

    Updated WebcamTest camera stream to return saturated version of input

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WebcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WebcamTest.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -15,10 +16,15 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @TeleOp
 public class WebcamTest extends OpMode {
     OpenCvCamera camera;
     DetectionPipeline pipeline = new DetectionPipeline();
+    private long counter = 0;
 
     @Override
     public void init() {
@@ -45,31 +51,75 @@ public class WebcamTest extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addData("Largest channel", pipeline.getLastResult());
+        try {
+            telemetry.addData("Largest channel", pipeline.getLastResult());
+            telemetry.addData("Count", counter);
+            counter++;
+        } catch (Exception e) {
+            telemetry.addData("Exception", e.getCause());
+            telemetry.update();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+//        try {
+//            Thread.sleep(50);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        Double[] vals = pipeline.getHsvVals();
+//        telemetry.addData("Hue", vals[0]);
+//        telemetry.addData("Sat", vals[1]);
+//        telemetry.addData("Val", vals[2]);
     }
 
     static class DetectionPipeline extends OpenCvPipeline {
-        Mat bgrMat = new Mat();
-        Mat bMat = new Mat(); // Channel 0
-        Mat gMat = new Mat(); // Channel 1
-        Mat rMat = new Mat(); // Channel 2
-        Mat all = new Mat();
-
         private static final float[] midPos = {1f/2f, 1f/2f};
 
-        int channelIndexLast = -1;
+        private int channelIndexLast = -1;
+//        private Double[] hsvVals;
 
         @Override
         public Mat processFrame(Mat input) {
-            Imgproc.cvtColor(input, bgrMat, Imgproc.COLOR_RGB2BGR);
-            Core.extractChannel(bgrMat, bMat, 0);
-            Core.extractChannel(bgrMat, gMat, 1);
-            Core.extractChannel(bgrMat, rMat, 2);
+            Mat bgrMat = new Mat();
+            Mat bMat = new Mat(); // Channel 0
+            Mat gMat = new Mat(); // Channel 1
+            Mat rMat = new Mat(); // Channel 2
 
-            rMat.copyTo(all);//copies mat object
+            Mat hsvMat = new Mat();
+            Mat hMat = new Mat(); // Channel 0
+//            Mat sMat = new Mat(); // Channel 1
+//            Mat vMat = new Mat(); // Channel 2
+
+            Mat all = new Mat();
+
+
+//            Imgproc.cvtColor(input, bgrMat, Imgproc.COLOR_RGB2BGR);
+            Core.extractChannel(input, rMat, 0);
+            Core.extractChannel(input, gMat, 1);
+            Core.extractChannel(input, bMat, 2);
+
+            // Do HSV stuff, crank S and V
+            Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
+            Core.extractChannel(hsvMat, hMat, 0);
+//            Core.extractChannel(hsvMat, sMat, 1);
+//            Core.extractChannel(hsvMat, vMat, 2);
+            Mat ones = new Mat(input.rows(), input.cols(), hMat.type(), new Scalar(255));
+
+            List<Mat> matList = Arrays.asList(
+                    hMat,
+                    ones,
+                    ones
+            );
+
+            Core.merge(matList, all);
+
+//            rMat.copyTo(all);//copies mat object
 
             //get values from frame
-            double[] pixMid = rMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]));//gets value at circle
+//            double[] pixMid = rMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]));//gets value at circle
 //            valMid = (int)pixMid[0];
 
             //create three points
@@ -105,6 +155,15 @@ public class WebcamTest extends OpMode {
 
             channelIndexLast = maxAt;
 
+            // Test code to get HSV vals
+
+//            this.hsvVals = new Double[] {
+//                    hMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]))[0],
+//                    sMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]))[0],
+//                    vMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]))[0]
+//            };
+
+            Imgproc.cvtColor(all, all, Imgproc.COLOR_HSV2RGB);
             return all;
             // return input;
         }
@@ -112,5 +171,9 @@ public class WebcamTest extends OpMode {
         public int getLastResult() {
             return channelIndexLast;
         }
+
+//        public Double[] getHsvVals() {
+//            return hsvVals;
+//        }
     }
 }
