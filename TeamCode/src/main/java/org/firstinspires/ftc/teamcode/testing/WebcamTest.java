1ef1276f0d651818c75126c629bff0ae4e09ee31
Delta11225 <lcsdeltarobotics@gmail.com>
2022-09-28 10:24:22 -0400

    Fixed openCV detection. Ready for competition use.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WebcamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/WebcamTest.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode.testing;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -16,125 +16,88 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
-@TeleOp
-public class WebcamTest extends OpMode {
-    OpenCvCamera camera;
-    DetectionPipeline pipeline = new DetectionPipeline();
-    private long counter = 0;
-
-    private Position pos;
+@Autonomous
+public class WebcamTest extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    OpenCvCamera webcam;
 
     @Override
-    public void init() {
-        telemetry.addData("Status", "Initialising");
-        telemetry = new TelemWrapper(telemetry, FtcDashboard.getInstance().getTelemetry());
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam1"); // TODO check this
-        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+    public void runOpMode() {
 
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        telemetry = new TelemWrapper(telemetry, FtcDashboard.getInstance().getTelemetry());
+        // servoTest = hardwareMap.get(Servo.class, "servoTest");
+        // servoTest.setPosition(0);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        DetectionPipeline pipeline = new DetectionPipeline();
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
-                FtcDashboard.getInstance().startCameraStream(camera, 0);
-                camera.setPipeline(pipeline);
-
+                webcam.setPipeline(pipeline);
+                //the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                webcam.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
+                FtcDashboard.getInstance().startCameraStream(webcam, 0);
             }
+
             @Override
             public void onError(int errorCode) {
-                telemetry.addData("Error code", errorCode);
-                telemetry.update();
+
             }
         });
+
+        waitForStart();
+        runtime.reset();
+
+        telemetry.addData("Raw vals (R, G, B)", Arrays.toString(pipeline.getRawVals()));
+        telemetry.addData("Result", pipeline.getLastPos());
+
+        telemetry.update();
+        sleep(1000);
     }
 
-    @Override
-    public void init_loop() {
-        telemetry.addData("Status", "Done initting, looping");
-
-        telemetry.addData("Largest channel", pipeline.getLastChannelMax());
-        telemetry.addData("Position", pipeline.getLastPos().toString());
-        Double[] vals = pipeline.getVals();
-
-        telemetry.addData("Red Val", vals[0]);
-        telemetry.addData("Green Val", vals[1]);
-        telemetry.addData("Blue Val", vals[2]);
-
-//        try {
-
-
-        telemetry.addData("Count", counter);
-        counter++;
-//        } catch (Exception e) {
-//            telemetry.addData("Exception", e.getCause());
-//            telemetry.update();
-//            try {
-//                Thread.sleep(10000);
-//            } catch (InterruptedException ex) {
-//                ex.printStackTrace();
-//            }
-//        }
-    }
-
-    @Override
-    public void start() {
-        telemetry.addData("Status", "Starting");
-        pos = pipeline.posLast;
-        camera.stopStreaming();
-        camera.stopRecordingPipeline();
-        camera.closeCameraDeviceAsync(() -> {});
-    }
-
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Running");
-        telemetry.addData("Pos", pos.toString());
-
-        telemetry.addData("Counter", counter);
-        counter++;
-
-//        try {
-//            Thread.sleep(50);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    static class DetectionPipeline extends OpenCvPipeline {
+    private static class DetectionPipeline extends OpenCvPipeline {
         private static final float[] midPos = {1f/2f, 1f/2f};
 
+        private static float rectHeight = 1f/8f;
+        private static float rectWidth =  1f/8f;
+
         private int channelIndexLast = -1;
-        private Double[] hsvVals = new Double[] {0d, 0d, 0d};
+        private Double[] rawVals = new Double[] {0d, 0d, 0d};
         private Position posLast = Position.HOW_ON_EARTH;
 
-        @Override
-        public Mat processFrame(Mat input) {
-            Mat rgbMat = new Mat();
-            Mat rMat = new Mat(); // Channel 0
-            Mat gMat = new Mat(); // Channel 1
-            Mat bMat = new Mat(); // Channel 2
+        Mat displayMat = new Mat();
+        Mat rMat = new Mat(); // Channel 0
+        Mat gMat = new Mat(); // Channel 1
+        Mat bMat = new Mat(); // Channel 2
 
-            Mat hsvMat = new Mat();
-            Mat hMat = new Mat(); // Channel 0
+        Mat hsvMat = new Mat();
+        Mat hMat = new Mat(); // Channel 0
 //            Mat sMat = new Mat(); // Channel 1
 //            Mat vMat = new Mat(); // Channel 2
 
-            Mat satMat = new Mat();
-            Mat all = new Mat();
+        Mat satMat = new Mat();
 
+        @Override
+        public Mat processFrame(Mat input) {
             // Do HSV stuff, crank S and V
             Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
             Core.extractChannel(hsvMat, hMat, 0);
             // We don't need these channels
 //            Core.extractChannel(hsvMat, sMat, 1);
 //            Core.extractChannel(hsvMat, vMat, 2);
+
+            // Get mat of all ones of the same size as input to set saturation and value (brightness)
+            // to max
             Mat ones = new Mat(input.rows(), input.cols(), hMat.type(), new Scalar(255));
 
+            // Put the mats in the proper order as merge() expects a list
             List<Mat> matList = Arrays.asList(
                     hMat,
                     ones,
@@ -142,29 +105,23 @@ public class WebcamTest extends OpMode {
             );
 
             Core.merge(matList, satMat);
-            satMat.copyTo(all);
+//            satMat.copyTo(all);
 
-            Imgproc.cvtColor(satMat, rgbMat, Imgproc.COLOR_HSV2RGB);
-            Core.extractChannel(rgbMat, rMat, 0);
-            Core.extractChannel(rgbMat, gMat, 1);
-            Core.extractChannel(rgbMat, bMat, 2);
-
-//            rMat.copyTo(all);//copies mat object
-
-            //get values from frame
-//            double[] pixMid = rMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]));//gets value at circle
-//            valMid = (int)pixMid[0];
+            // Convert back to RGB for display reasons
+            Imgproc.cvtColor(satMat, displayMat, Imgproc.COLOR_HSV2RGB);
+            Core.extractChannel(displayMat, rMat, 0);
+            Core.extractChannel(displayMat, gMat, 1);
+            Core.extractChannel(displayMat, bMat, 2);
 
             // Create middle point
             Point pointMid = new Point((int)(input.cols()* midPos[0]), (int)(input.rows()* midPos[1]));
 
             // Draw circle on specified point
-            Imgproc.circle(all, pointMid,5, new Scalar( 255, 0, 0 ),1 );//draws circle
+            Imgproc.circle(displayMat, pointMid,5, new Scalar( 255, 0, 0 ),1 );//draws circle
 
-            float rectHeight = 1f / 8f;
-            float rectWidth = 1f / 8f;
+            // Draw rectangle around point
             Imgproc.rectangle(
-                    all,
+                    displayMat,
                     new Point(
                             input.cols()*(midPos[0]- rectWidth /2),
                             input.rows()*(midPos[1]- rectHeight /2)),
@@ -189,28 +146,19 @@ public class WebcamTest extends OpMode {
             }
 
             channelIndexLast = maxAt;
-            // I want to do a switch statement but I can't at level 8 I apologize for bad code
-            if (maxAt == 0) {
-                posLast = Position.THREE;
-            } else if (maxAt == 1) {
-                posLast = Position.ONE;
-            } else if (maxAt == 2) {
-                posLast = Position.TWO;
-            } else {
-                posLast = Position.HOW_ON_EARTH;
-            }
 
-            // Test code to get HSV vals
+            // A mapping between channel index (r,g,b) and position (1,2,3). Change it if mapping
+            // changes.
+            HashMap<Integer, Position> posMappings = new HashMap<>();
+            posMappings.put(0, Position.THREE);
+            posMappings.put(1, Position.ONE);
+            posMappings.put(2, Position.TWO);
 
-//            this.hsvVals = new Double[] {
-//                    hMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]))[0],
-//                    sMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]))[0],
-//                    vMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]))[0]
-//            };
+            posLast = posMappings.getOrDefault(maxAt, Position.HOW_ON_EARTH);
 
-            Imgproc.cvtColor(all, all, Imgproc.COLOR_HSV2RGB);
-            hsvVals = vals;
-            return all;
+            // Set these for debugging
+            rawVals = vals;
+            return displayMat;
             // return input;
         }
 
@@ -218,8 +166,8 @@ public class WebcamTest extends OpMode {
             return channelIndexLast;
         }
 
-        public Double[] getVals() {
-            return hsvVals;
+        public Double[] getRawVals() {
+            return rawVals;
         }
 
         public Position getLastPos() {
