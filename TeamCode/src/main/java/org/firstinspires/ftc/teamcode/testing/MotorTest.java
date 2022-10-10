f1ae406392dc78d6b19f41852c45ec5a9af1c484
Delta11225 <lcsdeltarobotics@gmail.com>
2022-10-10 10:09:04 -0400

    Added telemetry to MotorTest (RANDOM)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/MotorTest.java
@@ -13,6 +13,7 @@ public class MotorTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Hardware22 robot = new Hardware22(hardwareMap);
         waitForStart();
+        telemetry.addData("yrdy", 1);
         while (opModeIsActive()) {
             robot.frontLeft.setPower(1);
         }
