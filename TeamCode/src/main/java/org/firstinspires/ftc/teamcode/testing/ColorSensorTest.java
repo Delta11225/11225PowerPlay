73b9431884e7b7207935c16cff01a742e215b1b0
Delta11225 <lcsdeltarobotics@gmail.com>
2022-10-27 09:24:31 -0400

    Coded red rev color/distance sensor for cone detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ColorSensorTest.java
@@ -3,23 +3,39 @@ package org.firstinspires.ftc.teamcode.testing;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
 public class ColorSensorTest extends OpMode {
-    NormalizedColorSensor sensor;
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
 
     @Override
     public void init() {
-        sensor = hardwareMap.get(NormalizedColorSensor.class, "color_sensor");
+        sensorColor = hardwareMap.get(ColorSensor.class, "colordistance_sensor");
+        sensorDistance = hardwareMap.get(DistanceSensor.class,"colordistance_sensor");
     }
 
     @Override
     public void loop() {
-        telemetry.addData("Color vals, r", sensor.getNormalizedColors().red);
-        telemetry.addData("Color vals, g", sensor.getNormalizedColors().green);
-        telemetry.addData("Color vals, b", sensor.getNormalizedColors().blue);
-        telemetry.addData("Color vals, a", sensor.getNormalizedColors().alpha);
+        if (sensorColor.red() > sensorColor.blue()) {
+            telemetry.addLine("Red");
+        }
+        if (sensorColor.blue() > sensorColor.red()) {
+            telemetry.addLine("Blue");
+        }
+        if (sensorDistance.getDistance(DistanceUnit.CM) < 3) {
+            telemetry.addLine("Ready to grab");
+        }
+
+
+       // telemetry.addData("Color vals, r", sensorColor.red());
+       // telemetry.addData("Color vals, g", sensorColor.green());
+       // telemetry.addData("Color vals, b", sensorColor.blue());
+       // telemetry.addData("Distance(cm)", sensorDistance.getDistance(DistanceUnit.CM));
         telemetry.update();
     }
 }
