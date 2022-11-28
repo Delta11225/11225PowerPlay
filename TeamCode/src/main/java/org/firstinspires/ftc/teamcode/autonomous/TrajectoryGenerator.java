55b396c0f91a314fc91fddcdf187757c0d1888d3
Delta11225 <lcsdeltarobotics@gmail.com>
2022-11-28 08:54:26 -0500

    Worked on adding trajectories to traj generator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TrajectoryGenerator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TrajectoryGenerator.java
@@ -6,6 +6,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
@@ -99,27 +100,25 @@ public class TrajectoryGenerator {
                             robot.rightClaw.setPosition(Constants.rightClawClosed);
                             robot.leftClaw.setPosition(Constants.leftClawClosed);
                         })
+                        .strafeTo(new Vector2d(-12.5, 65))
+                        .splineToConstantHeading(new Vector2d(-10, 57.1), Math.toRadians(270))
                         .addDisplacementMarker(() -> {
-                            robot.linearSlide.setTargetPosition(Constants.liftEncoderLow);
-                            robot.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                            robot.linearSlide.setPower(1);
-                        });
-                gen.splineToLinearHeading(new Pose2d(-25, 53, Math.toRadians(300)), Math.toRadians(300))
-                        .addDisplacementMarker(() -> {
-                            robot.leftClaw.setPosition(Constants.leftClawOpen);
-                            robot.rightClaw.setPosition(Constants.rightClawOpen);
-                        })
-                        .splineToLinearHeading(new Pose2d(-35, 60, Math.toRadians(270)), Math.toRadians(270))
-                        .addDisplacementMarker(() -> {
+                            // Should be high encoder
                             robot.linearSlide.setTargetPosition(0);
                             robot.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                             robot.linearSlide.setPower(1);
                         })
-                        .strafeTo(new Vector2d(-14.2, 58.5))
-                        .splineToConstantHeading(new Vector2d(-12.2, 58.5), Math.toRadians(270))
-                        .splineToConstantHeading(new Vector2d(-12.2, 9), Math.toRadians(270))
-                        .turn(Math.toRadians(-90))
-//                    .splineTo(new Vector2d(-14.2, 10), Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-5.6, 31, Math.toRadians(300)), Math.toRadians(300))
+                        .addDisplacementMarker(() -> {
+                            robot.leftClaw.setPosition(Constants.leftClawOpen);
+                            robot.rightClaw.setPosition(Constants.rightClawOpen);
+                            robot.linearSlide.setTargetPosition(0);
+                            robot.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                            robot.linearSlide.setPower(1);
+                        })
+                        .splineToLinearHeading(new Pose2d(-13, 30, Math.toRadians(270)), Math.toRadians(270))
+
+                        .splineToLinearHeading(new Pose2d(-16, 12, Math.toRadians(180)), Math.toRadians(180))
                         .addDisplacementMarker(() -> {
                             robot.linearSlide.setTargetPosition(Constants.liftEncoderConeStack[0]);
                             robot.linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
