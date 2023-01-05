e7561d206f71862b281b58d6f52f8d227a8ccd37
Delta11225 <lcsdeltarobotics@gmail.com>
2023-01-05 08:14:48 -0500

    Worked on test class

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTest.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTest.java
@@ -17,38 +17,49 @@ public class MeepMeepTest {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(57.5 * 0.9, 30, 5.398889 * 0.9, Math.toRadians(180), 12.12)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-39, 70-(12.25/2.0), Math.toRadians(270)))
-////                                // Right parking (3)
-//                                .strafeRight(35-16)
-//                                .splineToConstantHeading(new Vector2d(-59, 36), Math.toRadians(270))
-                                .setTurnConstraint(30, 2)
-                                .strafeTo(new Vector2d(-12.5, 65))
-                                .splineToConstantHeading(new Vector2d(-10, 57.1), Math.toRadians(270))
-                                .addDisplacementMarker(() -> {})
-                                .splineToLinearHeading(new Pose2d(-5.6, 31, Math.toRadians(300)), Math.toRadians(300))
-                                .addDisplacementMarker(() -> {})
-                                // Back up
-                                .splineToLinearHeading(new Pose2d(-13, 30, Math.toRadians(270)), Math.toRadians(270))
+                        drive.trajectorySequenceBuilder(new Pose2d(-40, 70 - (12.25 / 2.0), Math.toRadians(270)))
+//                            Pose2d startPose = new Pose2d(-40, 70 - (12.25 / 2.0), Math.toRadians(270));
+              .setTurnConstraint(60, 5)
+              .addDisplacementMarker(() -> {
 
-                                .splineToLinearHeading(new Pose2d(-16, 12, Math.toRadians(180)), Math.toRadians(180))
-//                                .turn(Math.toRadians(-90))
-//                                .splineTo(new Vector2d(-14.2, 10), Math.toRadians(180))
-                                .addDisplacementMarker(() -> {})
-                                .splineToConstantHeading(new Vector2d(-59, 12), Math.toRadians(180))
-                                .addDisplacementMarker(() -> {})
-                                .waitSeconds(0.75)
-                                .addDisplacementMarker(() -> {})
-                                .back(20)
-                                .addDisplacementMarker(() -> {})
-                                .splineToLinearHeading(new Pose2d(-28.5, 8.5, Math.toRadians(300)), Math.toRadians(300))
-                                .addDisplacementMarker(() -> {})
-                                .back(10)
-//                                // parking 1
-//                                .splineToLinearHeading(new Pose2d(-13, 13, Math.toRadians(270)), Math.toRadians(0))
-//                                // parking 2
-//                                .splineToLinearHeading(new Pose2d(-36, 13, Math.toRadians(270)), Math.toRadians(90))
-////                              // parking 2
-//                                .splineToLinearHeading(new Pose2d(-57, 13, Math.toRadians(270)), Math.toRadians(180))
+              })
+              .strafeTo(new Vector2d(-12.5, 65))
+              .splineToConstantHeading(new Vector2d(-10, 57.1), Math.toRadians(270))
+              .addDisplacementMarker(() -> {
+
+              })
+              .splineToLinearHeading(new Pose2d(-5.6, 31, Math.toRadians(300)), Math.toRadians(300))
+              .addDisplacementMarker(() -> {
+
+              })
+
+              // Back up
+              .splineToLinearHeading(new Pose2d(-13, 30, Math.toRadians(270)), Math.toRadians(270))
+
+              // Go to turn
+              .splineToLinearHeading(new Pose2d(-16, 12, Math.toRadians(180)), Math.toRadians(180))
+              .addDisplacementMarker(() -> {
+
+              })
+              .splineToConstantHeading(new Vector2d(-60, 9), Math.toRadians(180))
+              .waitSeconds(0.25)
+              .addDisplacementMarker(() -> {
+              })
+              // Do not ever do this. This is a hack.
+              .forward(0.001)
+              .waitSeconds(0.25)
+              .addDisplacementMarker(() -> {
+              })
+              .back(20)
+              .addDisplacementMarker(() -> {
+              })
+              .splineToLinearHeading(new Pose2d(-28.5, 8.5, Math.toRadians(300)), Math.toRadians(300))
+              .addDisplacementMarker(() -> {
+              })
+              .back(10)
+              .addDisplacementMarker(() -> {
+              })
+              .forward(0.001)
                                 .build()
                 );
 
