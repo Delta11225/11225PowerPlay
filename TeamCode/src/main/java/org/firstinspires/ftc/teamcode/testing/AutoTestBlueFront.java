7f5caea1367327ae4d90f363fa68588747d3774f
Delta11225 <lcsdeltarobotics@gmail.com>
2023-01-05 16:21:17 -0500

    Created new autotest for blue back to test trajectories on. completed through first high junction delivery

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AutoTestBlueFront.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AutoTestBlueFront.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.testing;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -12,7 +11,7 @@ import org.firstinspires.ftc.teamcode.util.Constants;
 import org.firstinspires.ftc.teamcode.util.Hardware23;
 
 @Autonomous
-public class AutoTest extends LinearOpMode {
+public class AutoTestBlueFront extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         Hardware23 robot = new Hardware23(hardwareMap);
