fd2fd4dd80457df5977ccc06a35a3b9cab4e7425
Delta11225 <lcsdeltarobotics@gmail.com>
2023-01-18 08:54:20 -0500

    Implemented cone auto grab test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ColorSensorTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.testing;
 
+import android.graphics.Color;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -7,18 +9,23 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 
+import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.util.Constants;
+import org.firstinspires.ftc.teamcode.util.Hardware23;
 
 @TeleOp
-@Disabled
+//@Disabled
 public class ColorSensorTest extends OpMode {
     ColorSensor sensorColor;
-    DistanceSensor sensorDistance;
+    Hardware23 robot;
 
     @Override
     public void init() {
-        sensorColor = hardwareMap.get(ColorSensor.class, "colordistance_sensor");
-        sensorDistance = hardwareMap.get(DistanceSensor.class,"colordistance_sensor");
+        robot = new Hardware23(hardwareMap);
+        sensorColor = robot.colorSensor;
+//        sensorColor = hardwareMap.get(ColorSensor.class, "color_sensor");
+//        sensorDistance = hardwareMap.get(DistanceSensor.class,"color_sensor");
     }
 
     @Override
@@ -29,8 +36,13 @@ public class ColorSensorTest extends OpMode {
         if (sensorColor.blue() > sensorColor.red()) {
             telemetry.addLine("Blue");
         }
-        if (sensorDistance.getDistance(DistanceUnit.CM) <3) {
+        if (((DistanceSensor) sensorColor).getDistance(DistanceUnit.CM) <3) {
             telemetry.addLine("Ready to grab");
+            robot.rightClaw.setPosition(Constants.rightClawClosed);
+            robot.leftClaw.setPosition(Constants.leftClawClosed);
+        } else {
+            robot.rightClaw.setPosition(Constants.rightClawOpen);
+            robot.leftClaw.setPosition(Constants.leftClawOpen);
         }
 
 
