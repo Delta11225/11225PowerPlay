0ea10a009b7eb3b467bb3cbe88eb9ce3ea1106a6
Dvd <everythingexplained1234@gmail.com>
2023-02-22 18:39:35 -0500

    Cleaned up newclaw, could have caused errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/newclaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/newclaw.java
@@ -1,63 +1,59 @@
+package org.firstinspires.ftc.teamcode.testing;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp
+@Disabled
+public class newclaw extends LinearOpMode {
 
+    DcMotor linearSlide;
+    Servo rightClaw;
+    Servo leftClaw;
+    int holdPosition;
 
-    @TeleOp
-    @Disabled
-    public class newclaw extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
 
-        DcMotor linearSlide;
-        Servo rightClaw;
-        Servo leftClaw;
-        int holdPosition;
+        //linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
 
-        @Override
-        public void runOpMode() throws InterruptedException {
+        rightClaw = hardwareMap.get(Servo.class, "right_claw");
+        leftClaw = hardwareMap.get(Servo.class, "left_claw");
 
-            //linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
+        telemetry.addData("status", "Initialized");
+        telemetry.update();
 
-            rightClaw = hardwareMap.get(Servo.class, "right_claw");
-            leftClaw = hardwareMap.get(Servo.class, "left_claw");
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-            telemetry.addData("status", "Initialized");
-            telemetry.update();
+        // puts the motor in brake setting so that when motor power = 0 the motor will hold position instead of idling
+        //linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-            linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //puts the motor in run using encoder mode so that motors can run while tracking encoder values
+        //Must have this line after stop/reset encoders or motor can't run
+        //linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            // puts the motor in brake setting so that when motor power = 0 the motor will hold position instead of idling
-            //linearSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //waitForStart();
 
-            //puts the motor in run using encoder mode so that motors can run while tracking encoder values
-            //Must have this line after stop/reset encoders or motor can't run
-            //linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+       // while (opModeIsActive()) {
 
-            //waitForStart();
-
-           // while (opModeIsActive()) {
-
-                /////////////////////////////LINEAR SLIDE//////////////////////////////
+            /////////////////////////////LINEAR SLIDE//////////////////////////////
 
 
 ////////////////////GRABBER////////////////////////////////////////////////////////
 
-                // A button = open claw, b button = closed claw
-                if (gamepad1.a) {
-                    rightClaw.setPosition(1); // Right claw open
-                    leftClaw.setPosition(1); // Left claw open
-                }
-                if (gamepad1.b) {
-                    rightClaw.setPosition(0); // Right claw closed
-                    leftClaw.setPosition(0); // Left claw closed
-                }
-
-
-            }
+        // A button = open claw, b button = closed claw
+        if (gamepad1.a) {
+            rightClaw.setPosition(1); // Right claw open
+            leftClaw.setPosition(1); // Left claw open
+        }
+        if (gamepad1.b) {
+            rightClaw.setPosition(0); // Right claw closed
+            leftClaw.setPosition(0); // Left claw closed
         }
 
 
-
-
+    }
+}
